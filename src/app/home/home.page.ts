0e8251d66017fceeacfc133b8d8ece17add3fa7b
GregoryVi1 <Gregoryvieir@gmail.com>
2022-04-24 23:15:49 -0300

    :tada: Primeiro Commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { flushMicrotasks } from '@angular/core/testing';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+  altura;
+  peso;
+  imc;
+  res;
+  
+  cal(){
+     this.imc = this.peso / (this.altura * this.altura);
+     console.log(this.imc);
+
+    if (this.imc >= 18.5 && this.imc <= 24.9 ) {
+        return this.res = ('normal');
+    }
+      else if (this.imc >= 25 && this.imc <= 29.9)  {
+        return this.res = ("Sobrepeso");
+    }
+      else if ( this.imc >= 30 ) {
+        return this.res = ("Obesidade");
+    }
+      else if (this.imc >= 40) {
+        return this.res = ("Obesidade Grave");
+    }}}
+    
+  
+
